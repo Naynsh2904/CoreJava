5ce3a993543083988165c4f2ab1979afff3d89e2
Nayan Bhaskar Shivankar <128512412+Naynsh2904@users.noreply.github.com>
2024-12-19 17:09:15 +0530

    Adding Day_08 ClassWork (Function)

--- ClassWork.java
+++ ClassWork.java
@@ -1,139 +1,79 @@
-package Day_06;
+package Day_08;
 
 import java.util.Scanner;
 
-public class Homework {
-	public static void main(String[] args) {
-		Scanner scnr = new Scanner(System.in);
-		
-//		1. Write a program for addition of single array elements.
-		System.out.print("Enter the number of elements in the arrays: ");
-		int size = scnr.nextInt();
-			
-	    int[] array1 = new int[size];
-	    int[] array2 = new int[size];
-	    int[] sumArray = new int[size];
-		        
-	    System.out.println("Enter the elements of the first array:");
-	    for (int i = 0; i < size; i++) {
-	    	array1[i] = scnr.nextInt();
-	    }
-	    System.out.println("Enter the elements of the second array:");
-	    for (int i = 0; i < size; i++) {
-	    	array2[i] = scnr.nextInt();
-	    }
-	    for (int i = 0; i < size; i++) {
-	    	sumArray[i] = array1[i] + array2[i];
-	    }
-	    System.out.println("The resulting array after addition is:");
-	    for (int value : sumArray) {
-	    	System.out.print(value + " ");
-	    }
-	    System.out.println();
-	    
-//	    2. Write a program for addition of two arrays stored in third array
-	    System.out.println("Enter the number of elements in the arrays:");
-		int size2 = scnr.nextInt();
-		
-		int[] arry1 = new int[size2];
-		System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < size2; i++) {
-            arry1[i] = scnr.nextInt(); 
-        }
-        int sum = 0;
-        for (int num : arry1) {
-            sum += num;
-        }
-        System.out.println("The sum of the array elements is: " + sum);
-        
-//     	3. Write a program to search a particular element in array and display its position.
-        System.out.print("Enter the number of elements in the array: ");
-        int size3 = scnr.nextInt();
-
-        int[] array = new int[size];
-
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < size; i++) {
-            array[i] = scnr.nextInt();
-        }
-
-        System.out.print("Enter the element to search: ");
-        int searchElement = scnr.nextInt();
-
-        int position = -1; 
-        for (int i = 0; i < size; i++) {
-            if (array[i] == searchElement) {
-                position = i; 
-                break;       
-            }
-        }
-
-        if (position != -1) {
-            System.out.println("Element " + searchElement + " is found at position " + (position + 1)); 
-        } else {
-            System.out.println("Element " + searchElement + " is not found in the array.");
-        }
-        
-//      4. Write a program to store squares and cubes of the elements in another arrays and Display all the three arrays.
-        System.out.print("Enter the number of elements in the array: ");
-        int size4 = scnr.nextInt();
-
-        int[] array4 = new int[size4];
-        int[] squares = new int[size4];
-        int[] cubes = new int[size4];
-        
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < size4; i++) {
-            array4[i] = scnr.nextInt();
-        }
-        
-        for (int i = 0; i < size4; i++) {
-            squares[i] = array4[i] * array4[i];
-            cubes[i] = array4[i] * array4[i] * array4[i];
-        }
-        
-        System.out.println("Original Array: ");
-        for (int value : array2) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-
-        System.out.println("Squares Array: ");
-        for (int value : squares) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-
-        System.out.println("Cubes Array: ");
-        for (int value : cubes) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-        
-//      5. Write a program to store the addition of two arrays into third array and print third array.
-        System.out.print("Enter the number of elements in the arrays: ");
-        int size5 = scnr.nextInt();
-
-        int[] array5 = new int[size5];
-        int[] array3 = new int[size5];
-        int[] resultArray = new int[size5];
-
-        System.out.println("Enter the elements of the first array:");
-        for (int i = 0; i < size5; i++) {
-            array5[i] = scnr.nextInt();
-        }
-        System.out.println("Enter the elements of the second array:");
-        for (int i = 0; i < size5; i++) {
-            array3[i] = scnr.nextInt();
-        }
-
-        for (int i = 0; i < size5; i++) {
-            resultArray[i] = array5[i] + array3[i];
-        }
-
-        System.out.println("The resultant array after addition is:");
-        for (int value : resultArray) {
-            System.out.print(value + " ");
-        }
+public class ClassWork {
+	
+	public static String Name() {
+		return "Nayan";
 	}
+	
+	public static int avg (int a, int b, int c) {
+		return (a+b+c)/3;
+	}
+	
+	public static float areaOfCircle (int r) {
+		float pi = 3.14f;
+		return pi*r*r;
+	}
+	
+	public static int triangle (int a, int b, int c) {
+		return (a+b+c);
+	}
+	
+	public static void posNeg () {
+		System.out.print("Enter number for checking Positive or Negative: ");
+		Scanner scnr = new Scanner(System.in);
+		int num = scnr.nextInt();
+		if(num >0) {
+			System.out.println("number is positive");
+		}else {
+			System.out.println("Number is negative");
+		}
+	}
+	
+	public static void squareCube( int num) {
+		System.out.println("Square of "+ num + " is " + num*num);
+		System.out.println("Cube of "+ num + " is " + num*num*num);
+	}
+	
+	public static int Greater (int num1, int num2) {
+		if(num1> num2) {
+			return num1;
+		}else {
+			return num2;
+		}
+	}
+	
+	public static int react (int l, int w) {
+		return (2*(l+w));
+	}
+	
+	public static void main(String[] args) {
+//		1. Write a program to print your name using function.
+		System.out.println("My name is " + Name());
+		System.out.println();
+//		2. Write a program to print the average marks of three subjects of a student.
+		System.out.println("Average is "+ avg(88,76,65));
+		System.out.println();
+//		3. Write a program to calculate area of circle using function.
+		System.out.println("area of circle is "+ areaOfCircle(2));
+		System.out.println();
+//		4. Write a program to calculate perimeter of a triangle using function..
+		System.out.println("area of circle is "+ triangle(2,6,4));
+		System.out.println();
+//		5. Write a program to check whether the number is positive or negative using function
+		posNeg();
+		System.out.println();
+//		6. Write a program to print the square and cube of a number using function.
+		squareCube(5);
+		System.out.println();
+//		7. Write a program to check the greater number among two numbers using function.
+		System.out.print("Greater number is "+Greater(10,54));
+		System.out.println();
+//		8. Write a program to find the perimeter of rectangle using function.
+		System.out.println("the perimeter of rectangle is "+ react(10,20));
+		
+	}
+
 }
